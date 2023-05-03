fd8e69d8172e8efb52bf54d1f7d40c8349935a3d
kilimanki <www.slipperstrong@gmail.com>
2023-05-03 15:49:45 +0200

    test on succes

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+// GOdMjEZWABEhYLzZ;
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://dmytro:GOdMjEZWABEhYLzZ@cluster0.hpqcrkd.mongodb.net/contacts_read?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
